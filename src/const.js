5cbe86b89ee4d0822f63d82a2e082019df2d86e2
pendeldam <45753426+pendeldam@users.noreply.github.com>
2020-09-24 07:14:06 +0300

    removed unused const

--- src/const.js
+++ src/const.js
@@ -30,17 +30,6 @@ export const UpdateType = {
 
 export const EVENT_TYPES = [`taxi`, `bus`, `train`, `ship`, `transport`, `drive`, `flight`, `check-in`, `sightseeing`, `restaurant`];
 export const EVENT_DESTINATIONS = [`Paris`, `Amsterdam`, `Berlin`, `Prague`, `Rome`, `Barcelona`, `London`];
-export const EVENT_DESCRIPTION = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                  Cras aliquet varius magna, non porta ligula feugiat eget.
-                                  Fusce tristique felis at fermentum pharetra.
-                                  Aliquam id orci ut lectus varius viverra.
-                                  Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.
-                                  Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.
-                                  Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.
-                                  Sed sed nisi sed augue convallis suscipit in sed felis.
-                                  Aliquam erat volutpat.
-                                  Nunc fermentum tortor ac porta dapibus.
-                                  In rutrum ac purus sit amet tempus.`;
 
 export const EVENT_OFFERS = [
   {
